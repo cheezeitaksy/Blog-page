fe8a1cb7087e5ea79a7d25b6f321f68c330ea695
cheezeitaksy <kushinlovewithburrito@gmail.com>
2026-06-29 12:53:23 +0530

    added databse

--- Django_project/blog/views.py
+++ Django_project/blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
-posts=[
-   { "author":"Dishu pagli",
-    "date_posted":"30 of apr,2007",
-    "content":"Mein bhot gussa hu or meine line change krli h",
-    "title":"1st blog"},
-
-    { "author":"Yash",
-    "date_posted":"2 of may,2007",
-    "content":"second post made",
-    "title":"2nd blog"}
-
-]
+from .models import post
 
 def home(request):
     context={
-        "posts":posts
+        "posts":post.objects.all()
     }
     return render(request,'blog/blog.html',context)
 def about(request):
